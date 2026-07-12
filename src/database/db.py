f8c3ba7e53aef9186725dfc9fc21acbae9fb5ee8
Harsh Mundra <mundrah.work@gmail.com>
2026-07-12 13:59:48 +0530

    teacher authentication

--- src/database/db.py
+++ src/database/db.py
@@ -1,25 +1,29 @@
 from src.database.config import supabase
 import bcrypt
+ 
+def hash_pass(pwd):
+    return bcrypt.hashpw(pwd.encode(),bcrypt.gensalt()).decode()
 
-def hash_password(pwd):
-    return bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
-
-def check_pwd(password, hashed):
-    return bcrypt.checkpw(password.encode(), hashed.encode())
+def check_pass(pwd,hashed):
+    return bcrypt.checkpw(pwd.encode(),hashed.encode())
 
 def check_teacher_exists(username):
-    response = supabase.table("teachers").select("username").eq("username", username).execute()
-    return len(response.data)>0;
+    response=supabase.table("teachers").select("username").eq("username",username).execute()
+    return len(response.data)>0
 
-def create_teacher(username, name, password):
-    user = {"username" : username, "name" : name, "password" : hash_password(password)}
-    response = supabase.table("teachers").insert(user).execute()
+def create_teacher(username,password,name):
+    
+    data={"username":username,
+          "name":name,
+          "password":hash_pass(password)
+          }
+    response=supabase.table("teachers").insert(data).execute()
     return response.data
 
-def teacher_login(username, password):
-    response = supabase.table("teachers").select("*").eq("username", username).execute()
-    if(response.data):
-        teacher = response.data[0]
-        if(check_pwd(password, teacher['password'])):
+def teacher_login(username,password):
+    response=supabase.table("teachers").select("*").eq("username",username).execute()
+    if response.data:
+        teacher=response.data[0]
+        if check_pass(password,teacher['password']):
             return teacher
-        return None
+    return None
